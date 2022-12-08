9392d64fb362c53399b4fa2b523eabb3c5afb0c9
kaimoore2k1 <92667053+kaimoore2k1@users.noreply.github.com>
2022-12-08 22:19:52 +0700

    Update requestTable.ts

--- src/requestTable.ts
+++ src/requestTable.ts
@@ -2,4 +2,6 @@ const BFF = "http://localhost:8080/api";
 
 export const requestTable: any = {
     "getStairAPI": `${BFF}/stair`,
+    "getDataCol2API": `${BFF}/datacol2`,
+    "getTang123API": `${BFF}/tang123`,
 };
