6b0f5fff50562b8ce84312f05b4c14c306c7bb1a
chien.pham <chien.pham@spiraledge.com>
2022-12-15 17:12:43 +0700

    update render map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,7 +2,7 @@ import GeoJSONLayer from '@arcgis/core/layers/GeoJSONLayer';
 import SceneView from '@arcgis/core/views/SceneView';
 import Map from '@arcgis/core/Map';
 import { data } from './data';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { StyledMap } from './styled';
 
 // Define interface
@@ -17,40 +17,85 @@ interface IUITMap {
  */
 const UITMap = ({ props }: IUITMap) => {
     const viewDiv = useRef(null);
+    const [value, setValue] = useState([]);
+    const [status, setStatus] = useState(false);
+
     useEffect(() => {
+        console.log(value);
+        fetch('http://localhost:4000/api/getData')
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setValue(result);
+                    setStatus(true);
+                },
+                (err) => {}
+            );
+
         if (viewDiv.current) {
             const map = new Map({
                 basemap: 'topo-vector',
                 ground: 'world-elevation',
             });
 
-            data.forEach((item) => {
-                const geojsonLayer = new GeoJSONLayer({
-                    url: item.url,
-                });
-                geojsonLayer.renderer = {
-                    type: 'simple',
-                    symbol: {
-                        type: 'polygon-3d',
-                        symbolLayers: [
-                            {
-                                type: 'extrude',
-                                size: item.size,
-                                material: {
-                                    color: item.color,
+        
+            if (value.length) {
+                const geojsonLayerArr = new Array(value.length);
+                value.map((item:any,index) => { 
+                    const blob = new Blob([JSON.stringify(item.data)], {
+                        type: 'application/json',
+                    });
+                    const url = URL.createObjectURL(blob);
+                    const geojsonLayer = new GeoJSONLayer({
+                        url,
+                    });
+                    const x:any = {
+                        type: "simple",
+                        symbol: {
+                            type: "polygon-3d",
+                            symbolLayers: [
+                                {
+                                    type: "extrude",
+                                    size: Number(item.size),
+                                    material: {
+                                        color: item.color || "#fff",
+                                    },
                                 },
-                            },
-                        ],
-                    },
-                } as any;
-                map.layers.add(geojsonLayer);
-            });
+                            ],
+                        }
+                    }
+                    geojsonLayer.renderer = x;
+                    map.layers.add(geojsonLayer);
+                })
+            }
+
+            // data.forEach((item) => {
+            //     const geojsonLayer = new GeoJSONLayer({
+            //         url: item.url,
+            //     });
+            //     geojsonLayer.renderer = {
+            //         type: 'simple',
+            //         symbol: {
+            //             type: 'polygon-3d',
+            //             symbolLayers: [
+            //                 {
+            //                     type: 'extrude',
+            //                     size: item.size,
+            //                     material: {
+            //                         color: item.color,
+            //                     },
+            //                 },
+            //             ],
+            //         },
+            //     } as any;
+            //     map.layers.add(geojsonLayer);
+            // });
 
             const view = new SceneView({
                 container: viewDiv.current,
                 map: map,
                 camera: {
-                    position: [106.80249, 10.86985, 300],
+                    position: [106.80155361294294, 10.87203569911929, 150],
                     heading: 0,
                     tilt: 75,
                 } as any,
@@ -58,7 +103,7 @@ const UITMap = ({ props }: IUITMap) => {
 
             view.popup.defaultPopupTemplateEnabled = true;
         }
-    }, []);
+    }, [status]);
 
     return <StyledMap ref={viewDiv}></StyledMap>;
 };
